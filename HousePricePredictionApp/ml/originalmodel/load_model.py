4d2130b0b0cdeb6f7a82a80c9a079ac49748c4ef
haely <32823897+haely@users.noreply.github.com>
2025-01-12 17:30:06 -0800

    add predict logic (#7)
    \

--- HousePricePredictionApp/ml/originalmodel/load_model.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-try:
-    interpreter = tf.lite.Interpreter(model_path="home_price_model.tflite")
-    interpreter.allocate_tensors()
-    print("Model loaded successfully!")
-except Exception as e:
-    print(f"Model loading failed: {e}")
